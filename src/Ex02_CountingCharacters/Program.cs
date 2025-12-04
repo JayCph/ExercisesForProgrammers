562eb9b75a09bca4c7fd5dee885700893fc3ffb2
janus <janus.bre@gmail.com>
2025-12-04 19:11:32 +0100

    cleaning up

--- src/Ex02_CountingCharacters/Program.cs
+++ src/Ex02_CountingCharacters/Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.WriteLine("What is the input string?");
 string input = Console.ReadLine() ?? "";
-Console.WriteLine("The lengthhh of {0} is: {1}", input, input.Length);
+Console.WriteLine("The length of {0} is: {1}", input, input.Length);
