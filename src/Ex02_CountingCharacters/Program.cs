9227d968805d75fda2507628d8b2f618bc9d0ff5
janus <janus.bre@gmail.com>
2025-12-04 19:09:11 +0100

    Testing new ssh setup

--- src/Ex02_CountingCharacters/Program.cs
+++ src/Ex02_CountingCharacters/Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.WriteLine("What is the input string?");
 string input = Console.ReadLine() ?? "";
-Console.WriteLine("The testing some stuff, length of {0} is: {1}", input, input.Length);
+Console.WriteLine("The length of {0} is: {1}", input, input.Length);
