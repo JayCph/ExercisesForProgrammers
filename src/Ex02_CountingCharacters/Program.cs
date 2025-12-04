d1c1020fb78855dfc0a87e73286348a4251eb157
janus <janus.bre@gmail.com>
2025-12-04 18:49:48 +0100

    2nd Ex done

--- src/Ex02_CountingCharacters/Program.cs
+++ src/Ex02_CountingCharacters/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What is the input string?");
+string input = Console.ReadLine() ?? "";
+Console.WriteLine("The length of {0} is: {1}", input, input.Length);
